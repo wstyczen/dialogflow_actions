ea1f7f013326e907df877efbd57170e9bff6915c
wstyczen <wstyczen00@gmail.com>
2023-12-16 20:40:50 +0100

    Add a very simple node to rotate the robot

---
+++ src/human_interactions/utility/rotate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2.7
+import rospy
+from geometry_msgs.msg import Twist
+
+if __name__ == "__main__":
+    rospy.init_node("rotate_rico")
+
+    velocity_publisher = rospy.Publisher("/joy_vel", Twist, queue_size=1000)
+
+    def publish_rotation_velocity(rotation_velocity):
+        velocity = Twist()
+        velocity.angular.z = rotation_velocity
+        velocity_publisher.publish(velocity)
+
+    stop = lambda: publish_rotation_velocity(0.0)
+    rotate = lambda: publish_rotation_velocity(
+        rospy.get_param("~velocity", default=0.5)
+    )
+
+    loop_rate = rospy.Rate(30)
+    while True:
+        rotate()
+        # stop()
+
+        loop_rate.sleep()
